88114102a09ff1013d8817e27444c991fbadc335
shehwar-dev <shehwar.dev@gmail.com>
2024-05-01 17:27:56 +0500

    fix: added react ga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import ReactGA from 'react-ga';
+
+
+ReactGA.initialize('G-CEJKBYXCSB');
+
 
 function App() {
   return (
